8fe5bb0f4657ff14bb2bce4730ab941e3100251e
Jonathan Liu <65567794+32111jl@users.noreply.github.com>
2023-05-24 11:03:26 -0700

    added gif to frontend

--- wordhuntfrontend/src/App.js
+++ wordhuntfrontend/src/App.js
@@ -1,21 +1,28 @@
-import logo from './logo.svg';
+import logo from './wordhunt.gif';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="solver">
+      <header className="solver-header">
+        <img src={logo} className="solver-logo" alt="logo"/>
+        <h2>Get the optimal number of points in GamePigeon's WordHunt!</h2>
+        <h4>Start by entering your 16-letter board below.</h4>
+        <h6>Input reads board from left to right, top down.</h6>
+        <form>
+          <label>
+            Board:    
+            <input type="text" className="textInput" placeholder="your word here..."/>
+          </label>
+          <input type="submit" className="submitInput" value="Submit"/>
+        </form>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="about-link"
+          href="https://32111jl.github.io/johnnyliujj/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          About me!
         </a>
       </header>
     </div>
